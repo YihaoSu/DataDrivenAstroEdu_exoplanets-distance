34cf2af0cc4113080f384039fecf415c9ed84a2f
YihaoSu <yhsu@astro.ncu.edu.tw>
2021-04-07 16:07:36 +0800

    Arrange dataframe of distance APP

--- exoplanets_distance.py
+++ exoplanets_distance.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import astropy.units as u
 from astroquery.nasa_exoplanet_archive import NasaExoplanetArchive
 
 
@@ -7,21 +8,38 @@ from astroquery.nasa_exoplanet_archive import NasaExoplanetArchive
 def get_exoplanet_data():
     parameters = 'fpl_name,fst_dist,fpl_bmasse,fpl_orbper,fpl_rade,'
     parameters += 'fpl_disc,fpl_discmethod'
-    exoplanet_data = NasaExoplanetArchive.query_criteria(
-        table='compositepars', select=parameters).to_pandas()
+    data = NasaExoplanetArchive.query_criteria(
+        table='compositepars', select=parameters)
 
-    return exoplanet_data
+    data['distance_lyr'] = data['fst_dist'].to(u.lyr)
+    data['distance_au'] = data['fst_dist'].to(u.au)
+    data['distance_km'] = data['fst_dist'].to(u.km)
+    data = data.to_pandas()
+    data = data[data.fst_dist > 0]
+    renamed_columns_dict = {
+        'fpl_name': 'name',
+        'fst_dist': 'distance_pc',
+        'fpl_bmasse': 'mass',
+        'fpl_orbper': 'orbital_period',
+        'fpl_rade': 'radius',
+        'fpl_disc': 'discovery_year',
+        'fpl_discmethod': 'discovery_method'
+    }
+    data.rename(columns=renamed_columns_dict, inplace=True)
+
+    return data
 
 
 st.set_page_config(page_title='歸途 - 太陽系外行星篇', layout='wide')
 st.title('歸途 - 太陽系外行星篇')
-st.header('*縮短與星的距離 拼出專屬於你的歸途*')
-st.markdown('<hr>', unsafe_allow_html=True)
-main_subheader = st.subheader('載入遊戲中，請稍候...')
-exoplanet_data = get_exoplanet_data()
+
+with st.spinner('載入遊戲中，請稍候...'):
+    exoplanet_data = get_exoplanet_data()
 
 if not exoplanet_data.empty:
-    main_subheader.subheader(':scroll: 遊戲說明:')
+    st.header('*縮短與星的距離 拼出專屬於你的歸途*')
+    st.markdown('<hr>', unsafe_allow_html=True)
+    st.subheader(':scroll: 遊戲說明:')
 
     if st.sidebar.button('隨機產生'):
         st.dataframe(exoplanet_data.sample())
