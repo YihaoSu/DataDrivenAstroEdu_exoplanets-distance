a18f83d573eb171bb90b07290c1374814bf6f9fc
YihaoSu <yhsu@astro.ncu.edu.tw>
2021-04-09 16:29:45 +0800

    Add functionality to record selected exoplanet

--- exoplanets_distance.py
+++ exoplanets_distance.py
@@ -30,16 +30,33 @@ def get_exoplanet_data():
     return data
 
 
+@st.cache(allow_output_mutation=True)
+def generate_my_exoplanets(selected_exoplanet=None):
+    if selected_exoplanet is None:
+        return []
+    else:
+        my_exoplanets.append(selected_exoplanet)
+        return pd.DataFrame(my_exoplanets).reset_index(drop=True)
+
+
 st.set_page_config(page_title='歸途 - 太陽系外行星篇', layout='wide')
 st.title('歸途 - 太陽系外行星篇')
 
-with st.spinner('載入遊戲中，請稍候...'):
+with st.spinner('載入中，請稍候...'):
     exoplanet_data = get_exoplanet_data()
+    my_exoplanets = generate_my_exoplanets()
 
 if not exoplanet_data.empty:
-    st.header('*縮短與星的距離 拼出專屬於你的歸途*')
-    st.markdown('<hr>', unsafe_allow_html=True)
-    st.subheader(':scroll: 遊戲說明:')
+    st.header('*縮短與星的距離 繪製專屬於你的歸途*')
+    st.markdown('---')
+    st.subheader(':scroll: 說明:')
+
+    if st.sidebar.button('清掉紀錄重新開始'):
+        st.caching.clear_cache()
 
     if st.sidebar.button('隨機產生'):
-        st.dataframe(exoplanet_data.sample())
+        selected_exoplanet = exoplanet_data.sample().iloc[0]
+        distance_au = int(selected_exoplanet.distance_au)
+        st.text(f'你與星的距離: 約為地球和太陽距離的{distance_au}倍')
+        my_exoplanets = generate_my_exoplanets(selected_exoplanet)
+        st.dataframe(my_exoplanets)
